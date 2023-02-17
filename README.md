4dd4c851957b199fd0e3f6ade185e468ce5f734c
Stephen Rice <goatwipe@gmail.com>
2023-02-17 18:19:46 +0000

    removed unworking questions file read. Readme layout set

---
+++ README.md
@@ -0,0 +1,38 @@
+
+# Project - 1
+
+2
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Licence](#licence)
+4. [Contributors](#contributors)
+5. [Tests](#tests)
+6. [Questions](#questions)
+
+### [Installation](#installation)
+
+3
+
+### [Usage](#usage)
+
+4
+
+### [Licence](#licence)
+
+5
+
+### [Contributors](#contributors)
+
+6
+
+### [Tests](#tests)
+
+78
+
+### [Questions](#questions)
+
+9
+    
--- index.mjs
+++ index.mjs
@@ -1,46 +1,98 @@
 import inquirer from 'inquirer';
-import fs from 'fs';
+//import fs from "fs";
+import fs from "fs/promises"
 
-// let questions = [
-//     {
-//         type: 'inpout',
-//         name: 'userName',
-//         message: 'What is your name?',
-//     },
-//     {
-//         type: 'input',
-//         name: 'languages',
-//         message: 'What languages do you know?',
-//     },
-//     {
-//         type: 'input',
-//         name: 'preferred',
-//         message: 'What is your preferred method of communication?',
-//     },
-// ]
-let questionsTxt;
-try {
-    questionsTxt = fs.readFileSync('objTest.txt', { encoding: 'utf8' });
-} catch (err) {
-    // An error occurred
-    console.error(err);
-}
+let questionsText = await fs.readFile('./assets/questions.txt', { encoding: "utf8", flag: "r" });
+//let questionsArray = removeWhiteSpace(questionsText);
 
-let questions = JSON.parse(questionsTxt);
+let questionsArray =  JSON.parse(removeWhiteSpace(questionsText));
+console.log(questionsArray);
+// let {title, description, installation, usage, licence, contributors, tests, questions} = await inquirer
+//     .prompt([
+//         {
+//             type: 'input',
+//             name: 'title',
+//             message: "What's the title of the project?",
+//         },
+//         {
+//             type: 'input',
+//             name: 'description',
+//             message: "Describe the project:",
+//         },
+//         {
+//             type: 'input',
+//             name: 'installation',
+//             message: "How is the project installed?",
+//         },
+//         {
+//             type: 'input',
+//             name: 'usage',
+//             message: "Provide instructions for the project:",
+//         },
+//         {
+//             type: 'input',
+//             name: 'licence',
+//             message: "Select a licence.",
+//         },
+//         {
+//             type: 'input',
+//             name: 'contributors',
+//             message: "List the contributors to the Meproject:",
+//         },
+//         {
+//             type: 'input',
+//             name: 'tests',
+//             message: "Enter testing information",
+//         },
+//         {
+//             type: 'input',
+//             name: 'questions',
+//             message: "GitHub link:",
+//         },
+//     ]);
 
-inquirer
-    .prompt(questions)
-    .then((answers) => {
-        fs.writeFile('answers.txt', JSON.stringify(answers), (err) =>
-            err ? console.error(err) : markUp(answers)
-        );
-    });
+    
+//   let readmeDocument =  `
+// # Project - ${title}
 
+// ${description}
 
-function markUp(readmeAnswers) {
-    console.log(typeof readmeAnswers);
+// ## Table of Contents
 
-//    let preMark = JSON.parse(readmeAnswers.txt);
-    // let marked = readmeAnswers.map(obj => obj.markup = "cheese");
-    // console.log(marked);
+// 1. [Installation](#installation)
+// 2. [Usage](#usage)
+// 3. [Licence](#licence)
+// 4. [Contributors](#contributors)
+// 5. [Tests](#tests)
+// 6. [Questions](#questions)
+
+// ### [Installation](#installation)
+
+// ${installation}
+
+// ### [Usage](#usage)
+
+// ${usage}
+
+// ### [Licence](#licence)
+
+// ${licence}
+
+// ### [Contributors](#contributors)
+
+// ${contributors}
+
+// ### [Tests](#tests)
+
+// ${tests}
+
+// ### [Questions](#questions)
+
+// ${questions}
+//     `
+
+//     await fs.writeFile("README.md", readmeDocument)
+
+function removeWhiteSpace(str) {
+    return str.replace(/[\s\r\n]+/g, "");
 }
