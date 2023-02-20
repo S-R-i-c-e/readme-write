d2a8003df285c6a1a61eae1d60f2d9607e75c88a
Stephen Rice <goatwipe@gmail.com>
2023-02-20 19:43:48 +0000

    moved questions back to code so to employ inquirer filter and validate

--- index.mjs
+++ index.mjs
@@ -1,16 +1,91 @@
 import inquirer from 'inquirer';
-
 import fs from "fs/promises"
 
-let questionsTxt;
+/*
+ * Reading the questions from file requires they be parsed and that means the validate(), filter(), and when()
+ * functions cannot be read - so not really a good idea.
+ */
+// let questionsTxt;
+// try {
+//     questionsTxt = await fs.readFile('./assets/questions.txt', 'utf-8');
+// } catch (error) {
+//     console.error('there was an error reading questions.txt:', error.message);
+// }
+// let questions = JSON.parse(questionsTxt);
 
-try {
-    questionsTxt = await fs.readFile('./assets/questions.txt', 'utf-8');
-} catch (error) {
-    console.error('there was an error reading questions.txt:', error.message);
-}
-
-let questions = JSON.parse(questionsTxt);
+const questions = [
+    {
+        "type": "input",
+        "name": "title",
+        "message": "What's the title of the project?",
+    },
+    {
+        "type": "input",
+        "name": "description",
+        "message": "Describe the project:"
+    },
+    {
+        "type": "input",
+        "name": "installation",
+        "message": "How is the project installed?"
+    },
+    {
+        "type": "input",
+        "name": "usage",
+        "message": "Provide instructions for the project:"
+    },
+    {
+        "type": "list",
+        "name": "licence",
+        "message": "Select a licence.",
+        "choices": ["MIT",
+                "Apache 2.0"
+        ]
+    },
+    {
+        "type": "input",
+        "name": "contributors",
+        "message": "List the contributors to the project:"
+    },
+    {
+        "type": "input",
+        "name": "tests",
+        "message": "Enter testing information",
+        filter(testAnswer) {
+            if(testAnswer==="") {
+                return false;
+            }
+            return testAnswer;
+        }
+    },
+    {
+        "type": "input",
+        "name": "githubUser",
+        "message": "Please enter your Github username:"
+    },
+    {
+        "type": "input",
+        "name": "repository",
+        "message": "Please enter the name of the repository:"
+    },
+    {
+        "type": "confirm",
+        "name": "hasWebpage",
+        "message": "Does the repo have a github webpage?"
+    },
+    {
+        "type": "input",
+        "name": "email",
+        "message": "Enter your email address",
+        validate(emailAddr) {
+            const emailValidator = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if(!emailValidator.test(emailAddr)) {
+                return "please provide a valid address.";
+            }
+            return true;
+        }
+    }
+];
 
 let {title, description, installation, usage, licence, contributors, tests, githubUser, repository, hasWebpage, email} = await inquirer
     .prompt(questions);
@@ -98,8 +173,8 @@ function githubLinks(userName, repos, emailAddress, webpageExists) {
     return {user: linkGit, myEmail: linkEmail, myRepo: linkRepo, myWebpage: linkWebpage};
 }
 
-  let readmeDocument =  `
-# Project - ${title}
+let readmeDocument =  `
+# ${title}
 
 ${description}
 
@@ -143,7 +218,7 @@ ${myRepo}
 ${myWebpage}
 `
 
-    await fs.writeFile("README.md", readmeDocument)
+await fs.writeFile("README.md", readmeDocument);
 
 function removeWhiteSpace(str) {
     return str.replace(/[\s\r\n]+/g, "");
